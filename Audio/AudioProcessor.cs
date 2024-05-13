11a6ac04df5921714cb043b7a66d3b52b558a995
significantnose <yanucevich.d@gmail.com>
2024-05-14 01:37:05 +0300

    AudioProcessor.Consume method fix

--- Audio/AudioProcessor.cs
+++ Audio/AudioProcessor.cs
@@ -107,8 +107,6 @@ public class AudioProcessor : IAudioConsumer
                     return;
             }
         }
-
-        throw new NotImplementedException();
     }
 
     public void Flush()
